0d2ab8e7b8d5385443c51e1e72c4e523801b02a5
nick13-qfin <nicolapede@outlook.com>
2022-03-08 09:34:54 +0000

    Boost tests

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -12,3 +12,5 @@ int main()
 	cout << "Hello CMake." << endl;
 	return 0;
 }
+
+
---
+++ tests/test1.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE hello test
+#define BOOST_TEST_MAIN
+//#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(la) {
+    BOOST_CHECK_EQUAL(1, 1);
+}
