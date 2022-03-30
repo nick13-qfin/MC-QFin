5a9a23e69537185f4817e2c28ae3cfe55d9d64c3
NP Ubuntu <nicolapede@outlook.com>
2022-03-30 17:46:59 +0100

    mc_engine variadic constructor + test

--- lib/mc_engine.h
+++ lib/mc_engine.h
@@ -1,17 +1,34 @@
 #pragma once
 #include "evolution_schemes/evol_scheme_base.h"
+#include "mc_representation/time_line.h"
+#include "mc_representation/mc_path.h"
 #include <tuple>
 #include <memory>
 
 namespace mc
 {
-	template<ev_scheme_type ... E>
+	template<ev_scheme_type... E>
 	class mc_engine
 	{
 		std::tuple<std::unique_ptr<E>...> schemes_;
+        std::shared_ptr<timeline> timeline_;
+        Eigen::MatrixXd single_path_;
+        
 
 	public:
 		mc_engine(std::unique_ptr<E>&&... schemes)
-			: schemes_(std::make_tuple(std::move(schemes)...)) {}
+            : schemes_(std::make_tuple(std::move(schemes)...)) {}
+            
+            
+        void calculate(/*function repr , n_simulation*/)
+        {
+            
+        }
 	};
+    
+    template<ev_scheme_type... E>
+    auto make_mcengine(std::unique_ptr<E>&&... schemes)
+    {
+        return mc_engine<E...>(std::move(schemes)...);
+    }
 }
--- tests/test_mc_engine.cpp
+++ tests/test_mc_engine.cpp
@@ -4,6 +4,8 @@
 #include "../lib/mc_engine.h"
 #include "../lib/stochastic_processes/geometric_brownian_motion.h"
 #include "../lib/evolution_schemes/euler_scheme.h"
+#include "../lib/parameters/parameter.h"
+#include "../lib/interpolation/pwc_interpolator.h"
 #include <tuple>
 
 BOOST_AUTO_TEST_SUITE(mcengine)
@@ -28,9 +30,28 @@ BOOST_AUTO_TEST_CASE(tuple)
 	
 	auto gbm2 = std::make_unique<gbm_t>(0.01, 0.1, 0.0); 
 	auto scheme2 = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm2));
+    
+	mc::mc_engine< mc::euler_scheme<gbm_t>, mc::euler_scheme<gbm_t>> engine(std::move(scheme), std::move(scheme2));
 
-	//mc::mc_engine< mc::euler_scheme<gbm_t>> engine(std::move(scheme), std::move(scheme2));
+	BOOST_CHECK_EQUAL(0.0, 0.0);
 
+}
+
+BOOST_AUTO_TEST_CASE(tupledifferenttypes)
+{
+	using gbm_t = mc::geometric_brownian_motion<mc::constant_param, mc::constant_param>;
+	auto gbm = std::make_unique<gbm_t>(0.01, 0.2, 0.0, true);
+	auto scheme = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm));
+	
+    using param_t = mc::time_dep_param<utils::pwc_interp<utils::unchecked_boundaries>>;
+	using gbm2_t = mc::geometric_brownian_motion<param_t, mc::constant_param>;
+    auto x = std::vector<double>{0.0, 1.0};
+    auto t = std::vector<double>{0.0, 1.0};
+    auto mu = std::make_unique<param_t>(x, t);
+	auto gbm2 = std::make_unique<gbm2_t>(std::move(mu), std::make_unique<mc::constant_param>(0.1), 0.0); 
+	auto scheme2 = std::make_unique<mc::euler_scheme<gbm2_t>>(0, 1, 0.1, std::move(gbm2));
+    
+    auto mc_engine = mc::make_mcengine(std::move(scheme), std::move(scheme2));
 	BOOST_CHECK_EQUAL(0.0, 0.0);
 
 }
