60f07ed0e381438eb4f5fc1e3243866a7650caec
NP Ubuntu <nicolapede@outlook.com>
2022-03-31 15:27:36 +0100

    rationalising

--- tests/test_mc_engine.cpp
+++ tests/test_mc_engine.cpp
@@ -9,42 +9,27 @@
 #include <tuple>
 
 BOOST_AUTO_TEST_SUITE(mcengine)
+using gbm_t = mc::geometric_brownian_motion<mc::constant_param, mc::constant_param>;
+using param_t = mc::time_dep_param<utils::pwc_interp<utils::unchecked_boundaries>>;
+using gbm2_t = mc::geometric_brownian_motion<param_t, mc::constant_param>;
 
-BOOST_AUTO_TEST_CASE(singlescheme)
+BOOST_AUTO_TEST_CASE(constr_singlescheme)
 {
-	using gbm_t = mc::geometric_brownian_motion<mc::constant_param, mc::constant_param>;
 	auto gbm = std::make_unique<gbm_t>(0.01, 0.2, 0.0, true);
 	auto scheme = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm));
 
 	mc::mc_engine< mc::euler_scheme<gbm_t>> engine(std::move(scheme));
+    const bool is_mc_engine = std::is_same_v<mc::mc_engine< mc::euler_scheme<gbm_t>>, decltype(engine)>;
 
-	BOOST_CHECK_EQUAL(0.0, 0.0);
-
+	BOOST_CHECK_EQUAL(is_mc_engine, true);
 }
 
-BOOST_AUTO_TEST_CASE(tuple)
+
+BOOST_AUTO_TEST_CASE(constr_tuple)
 {
-	using gbm_t = mc::geometric_brownian_motion<mc::constant_param, mc::constant_param>;
 	auto gbm = std::make_unique<gbm_t>(0.01, 0.2, 0.0, true);
 	auto scheme = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm));
-	
-	auto gbm2 = std::make_unique<gbm_t>(0.01, 0.1, 0.0); 
-	auto scheme2 = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm2));
-    
-	mc::mc_engine< mc::euler_scheme<gbm_t>, mc::euler_scheme<gbm_t>> engine(std::move(scheme), std::move(scheme2));
 
-	BOOST_CHECK_EQUAL(0.0, 0.0);
-
-}
-
-BOOST_AUTO_TEST_CASE(tupledifferenttypes)
-{
-	using gbm_t = mc::geometric_brownian_motion<mc::constant_param, mc::constant_param>;
-	auto gbm = std::make_unique<gbm_t>(0.01, 0.2, 0.0, true);
-	auto scheme = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm));
-	
-    using param_t = mc::time_dep_param<utils::pwc_interp<utils::unchecked_boundaries>>;
-	using gbm2_t = mc::geometric_brownian_motion<param_t, mc::constant_param>;
     auto x = std::vector<double>{0.0, 1.0};
     auto t = std::vector<double>{0.0, 1.0};
     auto mu = std::make_unique<param_t>(x, t);
@@ -52,8 +37,25 @@ BOOST_AUTO_TEST_CASE(tupledifferenttypes)
 	auto scheme2 = std::make_unique<mc::euler_scheme<gbm2_t>>(0, 1, 0.1, std::move(gbm2));
     
     auto mc_engine = mc::make_mcengine(std::move(scheme), std::move(scheme2));
-	BOOST_CHECK_EQUAL(0.0, 0.0);
+    const bool is_mc_engine = std::is_same_v<mc::mc_engine< mc::euler_scheme<gbm_t>, mc::euler_scheme<gbm2_t>>, decltype(mc_engine)>;
+	BOOST_CHECK_EQUAL(is_mc_engine, true);
+}
 
+BOOST_AUTO_TEST_CASE(calculate)
+{
+	auto gbm = std::make_unique<gbm_t>(0.01, 0.2, 0.0, true);
+	auto scheme = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm));
+
+    auto x = std::vector<double>{0.0, 1.0};
+    auto t = std::vector<double>{0.0, 1.0};
+    auto mu = std::make_unique<param_t>(x, t);
+	auto gbm2 = std::make_unique<gbm2_t>(std::move(mu), std::make_unique<mc::constant_param>(0.1), 0.0); 
+	auto scheme2 = std::make_unique<mc::euler_scheme<gbm2_t>>(0, 1, 0.1, std::move(gbm2));
+    
+    auto mc_engine = mc::make_mcengine(std::move(scheme), std::move(scheme2));
+
+//     mc_engine.calculate();
+    
 }
 
 
