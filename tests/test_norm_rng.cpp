cb860a5c9672068d4622c021c05dda56bd401eef
nick13-qfin <nicolapede@outlook.com>
2022-03-31 22:14:03 +0100

    test rng - begin

---
+++ tests/test_norm_rng.cpp
@@ -0,0 +1,19 @@
+#define BOOST_TEST_MODULE normrng test
+
+#include <boost/test/unit_test.hpp>
+#include "../lib/random_numbers/norm_rng.h"
+
+BOOST_AUTO_TEST_SUITE(normrng)
+
+BOOST_AUTO_TEST_CASE(test1)
+{
+    mc::normal_rng rand{ 1 };
+    auto a = rand.next();
+    auto b = rand.next();
+    BOOST_CHECK_EQUAL(0.0, 0.0);
+}
+
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
