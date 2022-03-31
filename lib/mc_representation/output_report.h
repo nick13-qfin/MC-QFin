79db33f94628a070c62ffb83a9c94bed6f2c78bb
nick13-qfin <nicolapede@outlook.com>
2022-03-31 17:47:18 +0100

    added operator[] and getn

--- lib/mc_representation/output_report.h
+++ lib/mc_representation/output_report.h
@@ -6,12 +6,22 @@ namespace mc
     class mc_report
     {
         std::vector<double> results_;
-        
+
     public:
-        void add_result(double value) 
+        void add_result(double value)
         {
             results_.push_back(value);
         }
-        
-    }
+
+        double operator[](size_t i) const
+        {
+            return results_[i];
+        }
+
+        size_t get_n() const
+        {
+            return results_.size();
+        }
+
+    };
 }
