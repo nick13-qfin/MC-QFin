173ecc80a52bc95be0b529c0678bfc225495e642
NP Ubuntu <nicolapede@outlook.com>
2022-03-31 15:27:08 +0100

    definition

--- lib/mc_representation/output_report.h
+++ lib/mc_representation/output_report.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <vector>
 
+namespace mc
+{
+    class mc_report
+    {
+        std::vector<double> results_;
+        
+    public:
+        void add_result(double value) 
+        {
+            results_.push_back(value);
+        }
+        
+    }
+}
