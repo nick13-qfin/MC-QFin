1bec42b505e851339ca5ead8250431a91c0c07b6
nick13-qfin <nicolapede@outlook.com>
2022-04-01 22:09:28 +0100

    stopwatch

--- lib/utils/stopwatch.h
+++ lib/utils/stopwatch.h
@@ -1,10 +1,30 @@
 #pragma once
-include<chrono>
+#include<chrono>
 
-namespace mc
+namespace utils
 {
 	class stopwatch
 	{
+
+		std::chrono::high_resolution_clock::time_point start_;
+		std::chrono::high_resolution_clock::time_point end_;
+
+	public:
+		void start()
+		{
+			start_ = std::chrono::high_resolution_clock::now();
+		}
+
+		void stop()
+		{
+			end_ = std::chrono::high_resolution_clock::now();
+		}
+
+		double elapsed_millisec() const
+		{
+			std::chrono::duration<double, std::milli> res = end_ - start_;
+			return res.count();
+		}
 
 	};
 }
