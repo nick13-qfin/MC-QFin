ca02fef91018643dd758c3063ce2fda8d5e6d993
nick13-qfin <nicolapede@outlook.com>
2022-04-01 14:15:01 +0100

    BS-like example

--- tests/test_mc_engine.cpp
+++ tests/test_mc_engine.cpp
@@ -18,7 +18,7 @@ BOOST_AUTO_TEST_CASE(constr_singlescheme)
 	auto gbm = std::make_unique<gbm_t>(0.01, 0.2, 0.0);
 	auto scheme = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm));
 
-	mc::mc_engine< mc::euler_scheme<gbm_t>> engine(std::move(scheme));
+	mc::mc_engine< mc::euler_scheme<gbm_t>> engine(1, std::move(scheme));
     const bool is_mc_engine = std::is_same_v<mc::mc_engine< mc::euler_scheme<gbm_t>>, decltype(engine)>;
 
 	BOOST_CHECK_EQUAL(is_mc_engine, true);
@@ -36,7 +36,7 @@ BOOST_AUTO_TEST_CASE(constr_tuple)
 	auto gbm2 = std::make_unique<gbm2_t>(std::move(mu), std::make_unique<mc::constant_param>(0.1), 0.0); 
 	auto scheme2 = std::make_unique<mc::euler_scheme<gbm2_t>>(0, 1, 0.1, std::move(gbm2));
     
-    auto mc_engine = mc::make_mcengine(std::move(scheme), std::move(scheme2));
+    auto mc_engine = mc::make_mcengine(1, std::move(scheme), std::move(scheme2));
     const bool is_mc_engine = std::is_same_v<mc::mc_engine< mc::euler_scheme<gbm_t>, mc::euler_scheme<gbm2_t>>, decltype(mc_engine)>;
 	BOOST_CHECK_EQUAL(is_mc_engine, true);
 }
@@ -46,11 +46,13 @@ BOOST_AUTO_TEST_CASE(calculate)
 	auto gbm = std::make_unique<gbm_t>(0.01, 0.2, 1.0);
 	auto scheme = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm));
     
-    auto mc_engine = mc::make_mcengine(std::move(scheme));
+    auto mc_engine = mc::make_mcengine(1000, std::move(scheme));
 
 	mc::dummy_payoff payoff{};
 	auto result = mc_engine.calculate(payoff);
     
+	auto value = result.get_estimate();
+	BOOST_CHECK_EQUAL(0.0, 0.0);
 }
 
 
