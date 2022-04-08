21516ec7e6d2ea285add369c47f46ae127d22df6
nick13-qfin <nicolapede@outlook.com>
2022-04-08 22:39:09 +0100

    major changes

--- montecarlo/mc_engine.h
+++ montecarlo/mc_engine.h
@@ -23,52 +23,96 @@ namespace mc
         size_t n_steps_ = 0;
         size_t n_sims_ = 2;
         std::shared_ptr<timeline> timeline_; // probably not needed
+        mc_report output_;
+        unsigned int seed_ = 1;
 
-
-
-	public:
-		mc_engine(size_t n_sims, std::unique_ptr<E>&&... schemes)
-            : n_sims_{ n_sims }, schemes_(std::make_tuple(std::move(schemes)...))
+        void init(std::tuple<std::unique_ptr<E>...>& schemes)
         {
             std::apply([&](auto&... args) {((
-                n_diffusions_+=args->get_process().get_n_diffusions()), ...); }, schemes_);
+                n_diffusions_ += args->get_process().get_n_diffusions()), ...); }, schemes);
 
-            auto n_schemes = sizeof...(E);
-            std::vector<size_t> tmp(n_schemes, 0.0);
+            const size_t n_schemes = sizeof...(E);
+            std::vector<size_t> tmp(n_schemes, 0);
             size_t i = 0;
             std::apply([&](auto&... args) {((
-                tmp[i++] = args->get_n_times()), ...); }, schemes_);
+                tmp[i++] = args->get_n_times()), ...); }, schemes);
             n_steps_ = tmp[0];
             //check timeline in schemes is the same?
             i = 0;
             std::apply([&](auto&... args) {((
-                args->set_process_index(i++)), ...); }, schemes_);
+                args->set_process_index(i++)), ...); }, schemes);
+        }
+
+	public:
+        mc_engine(size_t n_sims, unsigned int seed, std::tuple<std::unique_ptr<E>...>& schemes)
+            : n_sims_{ n_sims }, seed_{seed}
+            //, schemes_(std::make_tuple(std::make_unique<E>(schemes)...))
+        {
+            init(schemes);
+        }
+
+		mc_engine(size_t n_sims, unsigned int seed, std::unique_ptr<E>&&... schemes)
+            : schemes_(std::make_tuple(std::move(schemes)...)),
+            n_sims_{ n_sims }, seed_{seed}
+        {
+            init(schemes_);
         }
             
         template<class payoff_t>
-        mc_report calculate(const payoff_t& payoff/*function repr , n_simulation*/)
+        void calculate(const payoff_t& payoff, mc_report& output, std::tuple<std::unique_ptr<E...>>& schemes)
         {
             Eigen::MatrixXd master_path(n_diffusions_, n_steps_);
             Eigen::MatrixXd wieners(n_diffusions_, n_steps_ - 1);
             mc_path path(master_path);
-            mc_report output{};
-            normal_rng2 rand{ 1 };
+            output.reset();
+            normal_rng2 rand{ seed_ };
 
             for (size_t n = 0; n < n_sims_; n++)
             {
                 rand.fill_matrix(wieners, n_diffusions_, n_steps_ - 1);
                 size_t i = 0;
-                std::apply([&](auto&... ts) {(ts->evolve(path, wieners.row(i++)), ...); }, schemes_);
+                std::apply([&](auto&... ts) {(ts->evolve(path, wieners.row(i++)), ...); }, schemes);
                 auto result = payoff(path);
                 output.add_result(result);
             }
-            return output;
+        }
+
+        template<class payoff_t>
+        void calculate(const payoff_t& payoff, mc_report& output)
+        {
+            calculate(payoff, output, schemes_);
+        }
+
+        template<class payoff_t>
+        void calculate(const payoff_t& payoff)
+        {
+            calculate(payoff, output_);
+        }
+
+        const mc_report& get_results() const
+        {
+            return output_;
         }
 	};
     
     template<ev_scheme_type... E>
-    auto make_mcengine(size_t n_sims, std::unique_ptr<E>&&... schemes)
+    auto make_mcengine(size_t n_sims, unsigned int seed, std::unique_ptr<E>&&... schemes)
     {
-        return mc_engine<E...>(n_sims, std::move(schemes)...);
-    }
+        return mc_engine<E...>(n_sims, seed, std::move(schemes)...);
+    };
+    
+    template<ev_scheme_type... E>
+    auto make_mcengine(size_t n_sims, unsigned int seed, std::tuple<std::unique_ptr<E>...>& schemes)
+    {
+        return mc_engine<E...>(n_sims, seed, schemes);
+    };
+
+
+    template<class payoff_t, ev_scheme_type... E>
+    void run_single_thread(const payoff_t& p, size_t n_sims, unsigned int seed, 
+        mc_report& output, std::tuple<std::unique_ptr<E>...>& schemes)
+    {
+        auto mcengine = make_mcengine(n_sims, seed, schemes);
+        mcengine.calculate(p, output, schemes);
+    };
 }
--- tests/test_mc_engine.cpp
+++ tests/test_mc_engine.cpp
@@ -20,7 +20,7 @@ BOOST_AUTO_TEST_CASE(constr_singlescheme)
 	auto gbm = std::make_unique<gbm_t>(0.01, 0.2, 0.0);
 	auto scheme = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm));
 
-	mc::mc_engine< mc::euler_scheme<gbm_t>> engine(1, std::move(scheme));
+	mc::mc_engine< mc::euler_scheme<gbm_t>> engine(1,1, std::move(scheme));
     const bool is_mc_engine = std::is_same_v<mc::mc_engine< mc::euler_scheme<gbm_t>>, decltype(engine)>;
 
 	BOOST_CHECK_EQUAL(is_mc_engine, true);
@@ -38,7 +38,7 @@ BOOST_AUTO_TEST_CASE(constr_tuple)
 	auto gbm2 = std::make_unique<gbm2_t>(std::move(mu), std::make_unique<mc::constant_param>(0.1), 0.0); 
 	auto scheme2 = std::make_unique<mc::euler_scheme<gbm2_t>>(0, 1, 0.1, std::move(gbm2));
     
-    auto mc_engine = mc::make_mcengine(1, std::move(scheme), std::move(scheme2));
+    auto mc_engine = mc::make_mcengine(1, 1,  std::move(scheme), std::move(scheme2));
     const bool is_mc_engine = std::is_same_v<mc::mc_engine< mc::euler_scheme<gbm_t>, mc::euler_scheme<gbm2_t>>, decltype(mc_engine)>;
 	BOOST_CHECK_EQUAL(is_mc_engine, true);
 }
@@ -48,13 +48,13 @@ BOOST_AUTO_TEST_CASE(calculate)
 	auto gbm = std::make_unique<gbm_t>(0.01, 0.2, 1.0);
 	auto scheme = std::make_unique<mc::euler_scheme<gbm_t>>(0, 1, 0.1, std::move(gbm));
     
-    auto mc_engine = mc::make_mcengine(1000000, std::move(scheme));
+    auto mc_engine = mc::make_mcengine(1000000, 1, std::move(scheme));
 	utils::stopwatch sw{};
 	mc::dummy_payoff payoff{};
 	sw.start();
-	auto result = mc_engine.calculate(payoff);
+	mc_engine.calculate(payoff);
 	sw.stop();
-	auto value = result.get_estimate();
+	auto value = mc_engine.get_results().get_estimate();
 	auto time = sw.elapsed_millisec();
 	BOOST_TEST_MESSAGE("value = " + std::to_string(value));
 	BOOST_TEST_MESSAGE("time = " + std::to_string(time));
