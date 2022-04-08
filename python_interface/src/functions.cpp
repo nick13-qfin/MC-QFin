a9fa3fd0b949f0dfed5ea5b7d02df75f518f6bf7
nick13-qfin <nicolapede@outlook.com>
2022-04-08 16:05:48 +0100

    exported simple mc bs formula

--- python_interface/src/functions.cpp
+++ python_interface/src/functions.cpp
@@ -2,10 +2,34 @@
 #include <pybind11/stl.h>
 #include <../../random_numbers/include/norm_rng.h>
 #include <../../random_numbers/include/unif_rng.h>
+#include<../../montecarlo/mc_engine.h>
+#include<../../montecarlo/stochastic_processes/geometric_brownian_motion.h>
+#include<../../montecarlo/evolution_schemes/euler_scheme.h>
+#include<../../montecarlo/utils/stopwatch.h>
 
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+std::vector<double> black_scholes_mc(double S, double K, double T, double r, double sigma,
+    unsigned int seed, double dt, size_t n_mc)
+{
+    using gbm_t = mc::geometric_brownian_motion<mc::constant_param, mc::constant_param>;
+    auto gbm = std::make_unique<gbm_t>(r, sigma, S);
+    auto scheme = std::make_unique<mc::euler_scheme<gbm_t>>(0, T, dt, std::move(gbm));
+
+    auto mc_engine = mc::make_mcengine(n_mc, std::move(scheme));
+    utils::stopwatch sw{};
+    mc::dummy_payoff payoff{};
+    sw.start();
+    auto mcresult = mc_engine.calculate(payoff);
+    sw.stop();
+    auto value = mcresult.get_estimate();
+    auto time = sw.elapsed_millisec();
+    std::vector<double> result{ value, time };
+    return result;
+}
+
 
 std::vector<double> norm_rand(unsigned int seed, size_t n) 
 {
@@ -17,11 +41,11 @@ std::vector<double> norm_rand(unsigned int seed, size_t n)
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(rand, m) {
+PYBIND11_MODULE(qfin, m) {
     m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
-        .. currentmodule:: rand
+        .. currentmodule:: qfin
         .. autosummary::
            :toctree: _generate
            norm_rand
@@ -32,6 +56,9 @@ PYBIND11_MODULE(rand, m) {
         Returns a vector of norm(0,1) random numbers
     )pbdoc");
 
+    m.def("black_scholes_mc", &black_scholes_mc, R"pbdoc(
+        Returns a bs call price
+    )pbdoc");
     m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
         Subtract two numbers
         Some other explanation about the subtract function.
