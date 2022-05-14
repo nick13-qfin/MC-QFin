77a6cd6bcfbf231f30c08611f1a0fe84404b8c99
nick13-qfin <nicolapede@outlook.com>
2022-05-14 19:30:45 +0100

    interp2 base type

---
+++ interpolation/include/interp2d.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace interp
+{
+	template<class derived_interp>
+	class interp2d_base
+	{
+		const derived_interp& true_this() const
+		{
+			return static_cast<const derived_interp&>(*this);
+		}
+
+	public:
+		double interpolate(double t, double x) const
+		{
+			return true_this().interpolate(t, x);
+		}
+	};
+
+	// double1d interpolator used as a 2d interpolator
+
+}
